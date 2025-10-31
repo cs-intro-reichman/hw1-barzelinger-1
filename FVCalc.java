3aabc44b743147eb6f6c7618e35e57b521c63085
barzelinger <bar.zelinger@post.runi.ac.il>
2025-10-31 16:19:26 +0200

    uploading FVCalc file

--- FVCalc.java
+++ FVCalc.java
@@ -9,8 +9,11 @@ public class FVCalc {
 		// calculating future value using the formula FV = PV * (1 + r)^n
 		double futureValue = currentValue * Math.pow((1 + interestRate/100), years);	
 
-		System.out.println("After " + years + " years, " + "$" + (int) currentValue + 
-							" saved at " + interestRate + "%% will yeild $" + (int) futureValue); 
+		System.out.printf("After %d years, $%.0f saved at %.1f%% will yeild $%.0f%n", 
+								years, currentValue, interestRate, futureValue);
+
+		// System.out.println("After " + years + " years, " + "$" + (int) currentValue + 
+		// 					" saved at " + interestRate + "% will yeild $" + (int) futureValue); 
 	
 	}
 }
